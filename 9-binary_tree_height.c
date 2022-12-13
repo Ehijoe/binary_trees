dd91a353ec39a43944f484172f0f95f56a287fe1
Joseph Alikah <alikahjoseph@gmail.com>
2022-12-13 15:25:56 +0100

    Add task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Find the height of a binary tree
+ * @tree: The root node of the tree
+ *
+ * Return: The height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_h, right_h, max_height;
+
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_is_leaf(tree))
+		return (0);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+	max_height = (left_h > right_h) ? left_h : right_h;
+	return (max_height + 1);
+}
